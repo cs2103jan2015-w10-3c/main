263b34d59007584db48577ed96db9d197452f5e1
Joseph Gan <josephgankh@hotmail.com>
2015-03-10 00:20:12 +0800

    Adding of code to parse standard formatting input

--- V0.1/CheckMate V0.1/CheckMate V0.1/CMParser.cpp
+++ V0.1/CheckMate V0.1/CheckMate V0.1/CMParser.cpp
@@ -1,40 +1,155 @@
 #include "CMParser.h"
 
 string CMParser::getDescription(string str) {
-	return str;
+	return str.substr(0,str.rfind("from"));
 }
 
-string CMParser::getStartDate(string str) {
-	return str;
+string CMParser::getStartDate(string str, string type) {
+	string str1;
+	
+	local_time_input_facet* input_facet = new local_time_input_facet();
+	local_time_facet* output_facet = new local_time_facet();
+	stringstream ss;
+	
+	string inFormat = "%d/%m/%y";
+	string outFormat = "%d/%m/%y";
+	
+	ss.imbue(locale(locale::classic(), output_facet));
+	ss.imbue(locale(ss.getloc(), input_facet));
+	
+	input_facet->format(inFormat.c_str());
+	output_facet->format(outFormat.c_str());
+	
+	local_date_time ldt(not_a_date_time);
+	
+	if (type == "timed") {
+		str1 = str.substr(str.rfind("from")+5);
+	}
+	else {
+		str1 = str.substr(str.rfind("by")+3);
+	}
+
+	str1.erase(str1.find(" "));
+	ss.str(str1);
+	ss>>ldt;
+
+	ss.str("");
+	ss<<ldt;
+	
+	return ss.str();
 }
 
-string CMParser::getStartTime(string str) {
-	return str;
+string CMParser::getStartTime(string str, string type) {
+	string str1; 
+		
+	local_time_input_facet* input_facet = new local_time_input_facet();
+	local_time_facet* output_facet = new local_time_facet();
+	stringstream ss;
+	
+	string inFormat = "%H:%M";
+	string outFormat = "%R";
+	
+	ss.imbue(locale(locale::classic(), output_facet));
+	ss.imbue(locale(ss.getloc(), input_facet));
+	
+	input_facet->format(inFormat.c_str());
+	output_facet->format(outFormat.c_str());
+	
+	local_date_time ldt(not_a_date_time);
+
+	if (type == "timed") {
+		str1 = str.substr(str.rfind("from")+5);
+	}
+	else {
+		str1 = str.substr(str.rfind("by")+3);
+	}
+
+	str1 = str1.substr(str1.find(" ")+1);
+	
+	if (str1.find(" ")!=str1.npos){
+		str1.erase(str1.find(" "));
+	}
+
+	ss.str(str1);
+	ss>>ldt;
+
+	ss.str("");
+	ss<<ldt;
+	
+	return ss.str();
 }
 
 string CMParser::getEndDate(string str) {
-	return str;
+	string str1;
+	str1 = str.substr(str.rfind("to")+3);
+	str1.erase(str1.find(" "));
+
+	local_time_input_facet* input_facet = new local_time_input_facet();
+	local_time_facet* output_facet = new local_time_facet();
+	stringstream ss;
+	
+	string inFormat = "%d/%m/%y";
+	string outFormat = "%d/%m/%y";
+	
+	ss.imbue(locale(locale::classic(), output_facet));
+	ss.imbue(locale(ss.getloc(), input_facet));
+	
+	input_facet->format(inFormat.c_str());
+	output_facet->format(outFormat.c_str());
+	
+	local_date_time ldt(not_a_date_time);
+
+	ss.str(str1);
+	ss>>ldt;
+
+	ss.str("");
+	ss<<ldt;
+	
+	return ss.str();
 }
 
 string CMParser::getEndTime(string str) {
-	return str;
+	string str1 = str.substr(str.rfind("to")+3);
+	str1 = str1.substr(str1.find(" ")+1);
+	
+	if (str1.find(" ")!=str1.npos){
+	str1.erase(str1.find(" "));
+	}
+	
+	local_time_input_facet* input_facet = new local_time_input_facet();
+	local_time_facet* output_facet = new local_time_facet();
+	stringstream ss;
+	
+	string inFormat = "%H:%M";
+	string outFormat = "%R";
+	
+	ss.imbue(locale(locale::classic(), output_facet));
+	ss.imbue(locale(ss.getloc(), input_facet));
+	
+	input_facet->format(inFormat.c_str());
+	output_facet->format(outFormat.c_str());
+	
+	local_date_time ldt(not_a_date_time);
+
+	ss.str(str1);
+	ss>>ldt;
+
+	ss.str("");
+	ss<<ldt;
+	
+	return ss.str();
 }
 
 string CMParser::getCategory (string str){
-	return str;
-}
+	string str1;
 
-int main(){
-	CMParser test;
-	string tester;
-	test.getCategory(tester);
+	if (str.find("cat")!=str.npos) {
+		str1 = str.substr(str.find("cat")+1);
+		str1 = str1.substr(str1.find(" "));
+	}
 
-	boost::posix_time::ptime now = boost::posix_time::microsec_clock::local_time();
-	cout << now.time_of_day() << endl;
+	else
+		str1 = "nil";
 
-	string str;
-	cin >> str;
-	cout << str;
-
-	return 0;
+	return str1;
 }
--- V0.1/CheckMate V0.1/CheckMate V0.1/CMParser.h
+++ V0.1/CheckMate V0.1/CheckMate V0.1/CMParser.h
@@ -6,18 +6,20 @@
 #include <boost/date_time.hpp>
 
 using namespace std;
+using namespace boost::posix_time;
+using namespace boost::gregorian;
+using namespace boost::local_time;
 
 class CMParser {
 private:
 
 public:
 	string getDescription(string str);
-	string getStartDate(string str);
-	string getStartTime(string str);
+	string getStartDate(string str, string type);
+	string getStartTime(string str, string type);
 	string getEndDate(string str);
 	string getEndTime(string str);
 	string getCategory(string str);
-	
 
 };
 
